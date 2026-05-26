983619671a8db05b824231dd13ee3c54feb19667
Dhvani <dhvani@Dhvanis-MacBook-Air.local>
2026-05-26 23:28:30 +0530

    fixed vercel import paths

--- speakora-backend/debug_list_users.js
+++ speakora-backend/debug_list_users.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const User = require("./models/User");
+const User = require("./models/user");
 require("dotenv").config();
 
 console.log("Connecting to:", process.env.MONGO_URI);
